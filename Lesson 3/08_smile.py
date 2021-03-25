5bf153861843eb2da2706806ff3ea936a17e61c2
Smolenkov <smolenkovalexander@gmail.com>
2021-03-25 20:24:23 +0200

    lesson 08 smile - complete

--- Lesson 3/08_smile.py
+++ Lesson 3/08_smile.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-
+import simple_draw as sd
+sd.resolution = (1200 , 800)
+import random
 # (определение функций)
 import simple_draw
 
@@ -8,6 +10,26 @@ import simple_draw
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-# TODO здесь ваш код
 
-simple_draw.pause()
+def smile(x ,y , color):
+    point = sd.get_point(x , y)
+    point_left = sd.get_point( x - 20, y + 10)
+    point_right = sd.get_point( x + 20, y + 10)
+    point_left_mouth = sd.get_point( x - 50, y - 30)
+    point_right_mouth = sd.get_point( x + 50, y - 30)
+    sd.circle(point, 100, color, 2)
+    sd.circle(point_left, 10, color, 4)
+    sd.circle(point_right, 10, color, 4)
+    sd.line(point_left_mouth , point_right_mouth , color , 5)
+
+
+color = sd.random_color()
+x , y = 150 , 150
+smile (x , y , color)
+for _ in range(30):
+    x = int(random.random() * 1000 + 100)
+    y = int(random.random() * 600 + 100)
+    color = sd.random_color()
+    smile(x, y, color)
+
+sd.pause()
