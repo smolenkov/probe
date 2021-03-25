a79a87b93c69b8da863643753045e1cc2d260f57
Smolenkov <smolenkovalexander@gmail.com>
2021-03-25 19:55:40 +0200

    lesson 07 wall - complete

--- Lesson 3/07_wall.py
+++ Lesson 3/07_wall.py
@@ -15,10 +15,9 @@ start_y = 1
 width = 2
 for y in range(start_y, 801, 50):
     start_x = start_x * (-1)
-    for x in range (start_x * 50 ,1201, 100):
+    for x in range (start_x * 25 ,1201, 100):
         left_bottom_point = sd.get_point(x , y)
         right_top_point = sd.get_point(x+100 , y+50)
-        print(left_bottom_point , right_top_point)
         sd.rectangle(left_bottom_point, right_top_point , color=color , width=width)
 
 
