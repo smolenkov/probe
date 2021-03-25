06237d56b1415677dd2bd0e272e0929adc0e2f4c
Smolenkov <smolenkovalexander@gmail.com>
2021-03-25 11:50:31 +0200

    lesson 02 - envelop, bricks - complete

--- Lesson 3/02_envelop.py
+++ Lesson 3/02_envelop.py
@@ -8,29 +8,29 @@
 #
 # Результат проверки вывести на консоль (ДА/НЕТ)
 # Использовать только операторы if/elif/else, можно вложенные
-
+'''
 envelop_x, envelop_y = 10, 7
 paper_x, paper_y = 8, 9
 # проверить для
-#paper_x, paper_y = 9, 8
-#paper_x, paper_y = 6, 8
-#paper_x, paper_y = 8, 6
-#paper_x, paper_y = 3, 4
-#paper_x, paper_y = 11, 9
-#paper_x, paper_y = 9, 11
+paper_x, paper_y = 9, 8
+paper_x, paper_y = 6, 8
+paper_x, paper_y = 8, 6
+paper_x, paper_y = 3, 4
+paper_x, paper_y = 11, 9
+paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
-if envelop_x > paper_x:
-    print('Горизонтально По Х проходит')
+answer = 'Не поместится'
+
+print(f'x-{paper_x} y-{paper_y}')
+if envelop_x > paper_x: # Проверка горизонтальнрого вхождения бумаги
     if envelop_y > paper_y:
-        print('Горизонтально По Y тоже проходит, поместится')
-
-elif envelop_x > paper_y:
-    print('Вертикально по Х проходит')
+        answer = 'Поместится'
+if envelop_x > paper_y: # Проверка вертикального вхождения бумаги (повернули бумагу на 90 градусов)
     if envelop_y > paper_x:
-        print('Вертикально по Y проходит, поместится')
+        answer = 'Поместится'
+print(answer)
 
-else:
-    print('Не поместится')
+'''
 # TODO здесь ваш код
 
 # Усложненное задание, решать по желанию.
@@ -39,34 +39,49 @@ else:
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
-# brick_x, brick_y, brick_z = 11, 2, 10
-# brick_x, brick_y, brick_z = 10, 11, 2
-# brick_x, brick_y, brick_z = 10, 2, 11
-# brick_x, brick_y, brick_z = 2, 10, 11
-# brick_x, brick_y, brick_z = 2, 11, 10
-# brick_x, brick_y, brick_z = 3, 5, 6
-# brick_x, brick_y, brick_z = 3, 6, 5
-# brick_x, brick_y, brick_z = 6, 3, 5
-# brick_x, brick_y, brick_z = 6, 5, 3
-# brick_x, brick_y, brick_z = 5, 6, 3
-# brick_x, brick_y, brick_z = 5, 3, 6
-# brick_x, brick_y, brick_z = 11, 3, 6
-# brick_x, brick_y, brick_z = 11, 6, 3
-# brick_x, brick_y, brick_z = 6, 11, 3
-# brick_x, brick_y, brick_z = 6, 3, 11
-# brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
+hole_x, hole_y = 8, 9
+brick_x, brick_y, brick_z = 11, 10, 2
+brick_x, brick_y, brick_z = 11, 2, 10
+brick_x, brick_y, brick_z = 10, 11, 2
+brick_x, brick_y, brick_z = 10, 2, 11
+brick_x, brick_y, brick_z = 2, 10, 11
+brick_x, brick_y, brick_z = 2, 11, 10
+brick_x, brick_y, brick_z = 3, 5, 6
+brick_x, brick_y, brick_z = 3, 6, 5
+brick_x, brick_y, brick_z = 6, 3, 5
+brick_x, brick_y, brick_z = 6, 5, 3
+brick_x, brick_y, brick_z = 5, 6, 3
+brick_x, brick_y, brick_z = 5, 3, 6
+brick_x, brick_y, brick_z = 11, 3, 6
+brick_x, brick_y, brick_z = 11, 6, 3
+brick_x, brick_y, brick_z = 6, 11, 3
+brick_x, brick_y, brick_z = 6, 3, 11
+brick_x, brick_y, brick_z = 3, 6, 11
+brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
-'''
-# TODO здесь ваш код
-if hole_y <= brick_y:
-    if hole_y<=brick_z:
-        print('Нет')
-elif hole_y <= paper_y:
-    print('Нет')
 
-else:
-    print('Да')
-'''
+answer_brick = 'Не поместится'
+print(f'отверстие  x-{hole_x}  y-{hole_y}')
+print(f'x-{brick_x} y-{brick_y} z-{brick_z}')
+if hole_x > brick_x: # Проверка горизонтальнрого вхождения кирпича
+    if hole_y > brick_y:
+        answer_brick = 'Поместится'
+if hole_x > brick_y: # Проверка горизонтального повернутого вхождения кирпича
+    if hole_y > brick_x:
+        answer_brick = 'Поместится'
+if hole_x > brick_x: # Проверка продольного вхождения кирпича
+    if hole_y > brick_z:
+        answer_brick = 'Поместится'
+if hole_x > brick_z: # Проверка продольного повернутого вхождения кирпича
+    if hole_y > brick_x:
+        answer_brick = 'Поместится'
+if hole_x > brick_z: # Проверка вертикальгного вхождения кирпича
+    if hole_y > brick_y:
+        answer_brick = 'Поместится'
+if hole_x > brick_y: # Проверка вертикальгного повернутого вхождения кирпича
+    if hole_y > brick_z:
+        answer_brick = 'Поместится'
+
+
+
+print(answer_brick)
