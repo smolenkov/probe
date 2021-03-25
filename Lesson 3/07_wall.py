f1dc64398d88f37c6bd77822c1da42276dc9d7c4
Smolenkov <smolenkovalexander@gmail.com>
2021-03-25 19:33:29 +0200

    lesson 07 wall - not complete

--- Lesson 3/07_wall.py
+++ Lesson 3/07_wall.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
 # TODO здесь ваш код
+shift = 50
+color = sd.COLOR_YELLOW
+sd.resolution = (1200 , 800)
+start_x = 1
+start_y = 1
+width = 2
+for y in range(start_y, 801, 50):
+    start_x = start_x * (-1)
+    for x in range (start_x * 50 ,1201, 100):
+        left_bottom_point = sd.get_point(x , y)
+        right_top_point = sd.get_point(x+100 , y+50)
+        print(left_bottom_point , right_top_point)
+        sd.rectangle(left_bottom_point, right_top_point , color=color , width=width)
 
-simple_draw.pause()
+
+sd.pause()
