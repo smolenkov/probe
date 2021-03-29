1e5fbd8cf4c818c88396960fad0fa8e87c16a86c
Smolenkov <smolenkovalexander@gmail.com>
2021-03-29 16:54:58 +0300

    lesson 5-02 complete

--- Lesson 5/02_district.py
+++ Lesson 5/02_district.py
@@ -7,6 +7,23 @@
 
 # TODO здесь ваш код
 
+from district.central_street.house1.room1 import folks as room1_1
+from district.central_street.house1.room2 import folks as room1_2
+from district.central_street.house2.room1 import folks as room2_1
+from district.central_street.house2.room2 import folks as room2_2
+print('На районе живут: ',','.join((room1_1+room1_2+room2_1+room2_2)))
+# print(room1_1.folks)
+# print(room1_2.folks)
+# print(room2_1.folks)
+# print(room2_2.folks)
 
-
-
+# import os
+# tree = (os.walk('.'))
+# for i in tree:
+#     print( i )
+# # Set the directory you want to start from
+# rootDir = '.'
+# for dirName, subdirList, fileList in os.walk(rootDir):
+#     print('Found directory: %s' % dirName)
+#     for fname in fileList:
+#         print('\t%s' % fname)
