89aaa5c207bef3b85e6a77d1b9f11705819df5e0
Smolenkov <smolenkovalexander@gmail.com>
2021-03-29 14:40:40 +0300

    lesson 5-01 complete

--- Lesson 5/01_inhabitants.py
+++ Lesson 5/01_inhabitants.py
@@ -4,5 +4,11 @@
 # Формат: В комнате room_1 живут: ...
 
 # TODO здесь ваш код
+import room_1
+import room_2
+
+print ('В комнате 1 живут ', room_1.folks)
+print ('В комнате 2 живут ', room_2.folks)
+
 
 
