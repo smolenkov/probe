0cfbdd94a1a006abd8487995451ddcbc1849eedf
Smolenkov <smolenkovalexander@gmail.com>
2021-04-02 00:17:27 +0300

    snowfall object -complete

--- Lesson 7/01_snowfall.py
+++ Lesson 7/01_snowfall.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-
+from random import randint
 import simple_draw as sd
+sd.resolution = (1200,800)
 
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
@@ -9,25 +10,56 @@ import simple_draw as sd
 
 
 class Snowflake:
-    pass
+    def __init__(self):
+        self.x_snow = randint(20, 1180)
+        self.y_snow = 770
+        self.len_snow = randint(10, 30)
+        self.delta_x_snow = randint(0, 2)
+        self.delta_y_snow = randint(7, 10)
+        self.color_snow = (randint(0, 255), randint(0, 255), randint(0, 255))
+        self.down_snow = 0
 
-    # TODO здесь ваш код
+    def snow_draw(self):
+        self.point = sd.get_point(self.x_snow, self.y_snow)
+        sd.snowflake(center=self.point, length=self.len_snow, color=self.color_snow)
+
+    def snow_clear(self):
+        self.point = sd.get_point(self.x_snow, self.y_snow)
+        self.color_clear = sd.background_color
+        sd.snowflake(center=self.point, length=self.len_snow, color=self.color_clear)
+
+    def snow_shift(self):
+        self.x_snow += randint(-2, 2)
+        self.y_snow -= self.delta_y_snow
+
+    def checking_fall(self):
+        if self.y_snow < 20:
+            self.down_snow = 1
+            return False
 
 
-flake = Snowflake()
+flakes = []
+for _ in range (30):
+    flakes.append(Snowflake())
 
 while True:
-    flake.clear_previous_picture()
-    flake.move()
-    flake.draw()
-    if not flake.can_fall():
-        break
-    sd.sleep(0.1)
+    for flake in flakes:
+        flake.snow_clear()
+        flake.snow_shift()
+        flake.snow_draw()
+        flake.checking_fall()
+        if flake.down_snow == 1:
+            flakes.remove(flake)
+            flakes.append(Snowflake())
+            break
+        sd.sleep(0.001)
+        if sd.user_want_exit():
+            break
     if sd.user_want_exit():
         break
-
 # шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
 # flakes = get_flakes(count=N)  # создать список снежинок
+
 # while True:
 #     for flake in flakes:
 #         flake.clear_previous_picture()
