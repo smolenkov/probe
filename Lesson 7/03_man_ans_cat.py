8264f3d7c1d1e7505715ed8ec91bf8d5ddd83c66
Smolenkov <smolenkovalexander@gmail.com>
2021-04-03 17:39:54 +0300

    cat-man - complete

--- Lesson 7/03_man_ans_cat.py
+++ Lesson 7/03_man_ans_cat.py
@@ -24,6 +24,77 @@ from random import randint
 # что будет делать сегодня
 
 # Человеку и коту надо вместе прожить 365 дней.
+class Man:
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+
+    def __str__(self):
+        return 'Я - {}, сытость {}'.format(self.name, self.fullness)
+
+    def eat(self):
+        if self.house.food >= 10:
+            print(f'{self.name} поел')
+            self. fullness += 20
+            self.house.food -= 10
+
+    def game(self):
+        print(f'{self.name} поиграл в PS 5')
+        self.fullness -= 10
+
+    def take_a_cat(self, cat, house):
+        cat.house = house
+        self.fullness -= 10
+
+    def shopping(self):
+        if self.house.money >= 50:
+            print(f'{self.name} сходил в магазин')
+            self.house.money -= 50
+            self.house.food += 50
+            self.fullness -= 10
+        else:
+            print('Закончились деньги')
+
+    def work(self):
+        print(f'{self.name} сходил на работу')
+        self.house.money +=30
+        self.fullness -= 10
+
+
+    def clean_house(self):
+        print(f'{self.name} убрался в доме')
+        if self.house.mud >= 10:
+            self.house.mud -= 10
+        self.fullness -= 10
+
+    def man_act(self):
+        if self.fullness <=0:
+            print(f'{self.name} умер')
+            return
+        dice  = randint (1,4)
+        if self.fullness <=20:
+            self.eat()
+        if self.house.food <=20:
+            self.shopping()
+        elif self.house.money <= 50:
+            self. work()
+        elif dice == 1:
+            self.game()
+        elif dice == 2:
+            self.work()
+        elif dice == 3:
+            self.eat()
+        else:
+            self.clean_house()
+        dice_cat = randint(1,20)
+
+
+    def go_to_the_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        print(f'{self.name} Вьехал в дом')
 
 
 class Cat:
@@ -33,24 +104,27 @@ class Cat:
         self.fullness = 50
         self.house = None
 
+    def __str__(self):
+        return 'Я - кот {}, сытость {}'.format(self.name, self.fullness)
+
     def sleep(self):
-        print(f'кот{self.name} поспал')
+        print(f'кот {self.name} поспал')
         self.fullness -= 10
 
     def eat(self):
         if self.house.food >= 10:
-            print(f'кот{self.name} поел')
+            print(f'кот {self.name} поел')
             self.fullness += 20
             self.house.food -= 10
         else:
             print('нет еды')
 
     def scratch(self):
-        print(f'кот{self.name} поцарапал обои')
+        print(f'кот {self.name} поцарапал обои')
         self.fullness -= 10
         self.house.mud +=5
 
-    def aсt:
+    def cat_act(self):
         if self.fullness <= 0:
             print(f'кот {self.name} умер')
             return
@@ -61,7 +135,47 @@ class Cat:
             self.sleep()
         elif dice == 2:
             self.scratch()
+        else:
+            self.eat()
 
+class House:
+
+    def __init__(self):
+        self.food = 50
+        self.money = 0
+        self.mud = 0
+
+    def __str__(self):
+        return 'В доме еды осталось {}, денег осталось {}, грязи {}'.format(self.food, self.money, self.mud)
+
+citizens = [
+    Man(name='Леонардо'),
+    Man(name='Донателло'),
+    Man(name='Микеланджело')]
+
+cats = [Cat(name = 'Мурзик'),
+        Cat(name = 'Барсик'),
+        Cat(name = 'Персик')]
+
+sweet_home = House()
+i=0
+for i in range(0,3):
+    citizens[i].go_to_the_house(sweet_home)
+    citizens[i].take_a_cat(cats[i], sweet_home)
+
+
+for day in range(1, 366):
+    print('================ день {} =================='.format(day))
+    for citisen in citizens:
+        citisen.man_act()
+    for cat in cats:
+        cat.cat_act()
+    print('--- в конце дня ---')
+    for citisen in citizens:
+        print(citisen)
+    for cat in cats:
+        print(cat)
+    print(sweet_home)
 
 # Усложненное задание (делать по желанию)
 # Создать несколько (2-3) котов и подселить их в дом к человеку.
